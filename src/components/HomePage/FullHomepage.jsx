af5f27dc7b33138b9047dc0862be78ecde2022f6
Humayra-Adiba <tahsinadiba2024@outlook.com>
2025-01-06 14:41:44 +0600

    MyFeedback section done

--- src/components/HomePage/FullHomepage.jsx
+++ src/components/HomePage/FullHomepage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import AllTopic from './AllTopic'
 import FirstCarousel from './FirstCarousel'
 import Trends from './Trends'
+import MyFeedback from './MyFeedback'
 function FullHomepage() {
   return (
     <>
       <AllTopic/>
       <FirstCarousel/>
       <Trends/>
+      <MyFeedback/>
     </>
   )
 }
